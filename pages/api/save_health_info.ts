316335824b20c664b94eaa903a018a3822695901
VatsalGhaghda <ghaghdavatsal0@gmail.com>
2025-02-01 15:46:17 +0530

    File:save_health_info removed

--- pages/api/save_health_info.ts
+++ pages/api/save_health_info.ts
@@ -1,40 +1,40 @@
-import { PrismaClient } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from 'next';
+// import { PrismaClient } from "@prisma/client";
+// import type { NextApiRequest, NextApiResponse } from 'next';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const {
-      medicalConditions,
-      allergies,
-      pastSurgeries,
-      primaryDoctorName,
-      primaryDoctorContact,
-      insurancePolicyNumber,
-      preferredHospital,
-      personalInfoId,
-    } = req.body;
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === "POST") {
+//     const {
+//       medicalConditions,
+//       allergies,
+//       pastSurgeries,
+//       primaryDoctorName,
+//       primaryDoctorContact,
+//       insurancePolicyNumber,
+//       preferredHospital,
+//       personalInfoId,
+//     } = req.body;
 
-    try {
-      const healthInfo = await prisma.healthInfo.create({
-        data: {
-          medicalConditions,
-          allergies,
-          pastSurgeries,
-          primaryDoctorName,
-          primaryDoctorContact,
-          insurancePolicyNumber,
-          preferredHospital,
-          personalInfoId,
-        },
-      });
+//     try {
+//       const healthInfo = await prisma.healthInfo.create({
+//         data: {
+//           medicalConditions,
+//           allergies,
+//           pastSurgeries,
+//           primaryDoctorName,
+//           primaryDoctorContact,
+//           insurancePolicyNumber,
+//           preferredHospital,
+//           personalInfoId,
+//         },
+//       });
 
-      res.status(200).json({ success: true, data: healthInfo });
-    } catch (error) {
-      res.status(500).json({ success: false, error:"error occured" });
-    }
-  } else {
-    res.status(405).json({ success: false, error: "Method not allowed" });
-  }
-}
+//       res.status(200).json({ success: true, data: healthInfo });
+//     } catch (error) {
+//       res.status(500).json({ success: false, error:"error occured" });
+//     }
+//   } else {
+//     res.status(405).json({ success: false, error: "Method not allowed" });
+//   }
+// }
